7445df6815ed2e774cab5c6801447eb33a5c3f58
Zamoox <zamoox@gmail.com>
2020-05-24 23:55:45 +0300

    Updates

---
+++ precache-manifest.81a7a4ee3e0fcfa200696cfcf5581495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8c9578531ea5476d2961f7b627d99d",
+    "url": "/chuck-norris-app/index.html"
+  },
+  {
+    "revision": "22a93b9d5f387e2e695b",
+    "url": "/chuck-norris-app/static/css/main.ac6e87dd.chunk.css"
+  },
+  {
+    "revision": "dbc85680e5213493f87c",
+    "url": "/chuck-norris-app/static/js/2.d3272a3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris-app/static/js/2.d3272a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a93b9d5f387e2e695b",
+    "url": "/chuck-norris-app/static/js/main.232ac768.chunk.js"
+  },
+  {
+    "revision": "652b7c6aab99ca93b77d",
+    "url": "/chuck-norris-app/static/js/runtime-main.39fbdcdc.js"
+  }
+]);
